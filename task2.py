8675adedab06dd571a4233d281212ba272674de6
DiChenB00747156 <dz214162@dal.ca>
2017-10-20 15:17:06 -0300

    3rd commit

--- task2.py
+++ task2.py
@@ -7,20 +7,31 @@ weather = Weather()
 
 location = weather.lookup_by_location('dublin')
 condition = location.condition()
-#print (condition['text'])
+hi=0
+lo=0
+a=0
+b=0
+for forecasts in location.forecast():
+  # print(forecasts)
+   hi=int(forecasts['high'])
 
-# Get weather forecasts for the upcoming days.
+   if hi > b:
+      b=hi
+      print('high :',b)
 
 for forecasts in location.forecast():
-  #  print(forecasts)
+    lo = int(forecasts['low'])
+    print(lo)
+    if lo < a:
+        a = lo
+        print('low :', a)
+
+for forecasts in location.forecast():
     while forecasts['text'] == 'Rain':
-        print('It might rain on :',forecasts['day'])
+        print('It might rain on :', forecasts['day'])
         print(condition['text'])
         break
-    """
-    print ('condition:',forecasts['text'],end=" ")
-    print ("on",forecasts['date'])
-
-    print ((forecasts['high']))
-    print ('Low:',forecasts['low'])
-    """
+#print ('condition:',forecasts['text'],end=" ")     print ("on",forecasts['date'])
+ 
+ #    print ((forecasts['high']))
+ #    print ('Low:',forecasts['low'])
